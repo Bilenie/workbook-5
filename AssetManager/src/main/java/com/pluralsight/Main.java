6696692ba8890b56af6f861921f6be77e1be1570
Bilenie Tilahun <bilumekb@gmail.com>
2025-05-14 14:59:49 -0500

    create arrayList and add to my list assets.

--- AssetManager/src/main/java/com/pluralsight/Main.java
+++ AssetManager/src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         //arrayList of Asset object => vacation home and 2 vehicles  /myHouse,bilenieSuv
+  ArrayList<Asset> assets = new ArrayList <>();
+//(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer)
+        assets.add(new Vehicle ("My Rav4 Car", "01/02/2025" ,47000.00 , "Toyota", 2025,7000 ));
+        assets.add(new Vehicle ("My Second Car", "04/05/2024" ,35000.00 , "Honda", 2024,100000 ));
+
+//(String description, String dateAcquired, double originalCost, String address, int condition, int square foot, int lotSize)
+        assets.add(new House("My Primary House", "12/30/2025", 200000.0,"Garland Texas" , 1, 2000 , 5000));
+        assets.add(new House("My Vacation House", "12/30/2028", 200500.0,"South Lake Texas" , 2, 1500 , 4000));
 
         //loop through the list display (description of the object(  I will use toString method => date acquired, price we paid, value of the vehicle & the house )
         // Then loop through again the list & display (description of the object(  I will use toString method => address of house, year & make/mode of car.)
@@ -23,6 +31,6 @@ Vehicle vehicle = (Vehicle) myAssets.get(i);
 message = "Vehicle: " +
 vehicle.getYear() + " " + vehicle.getMakeModel();
 }*/
-        //ArrayList<Vehicle> myCar = ArrayList <>();
+
     }
 }
